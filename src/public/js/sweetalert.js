09cba1a29c26e3df121dbd0f8240c55d3361a0d3
Ethanolll <icedgreentea676@gmail.com>
2022-08-24 16:28:13 +0900

    feat: alert

--- src/public/js/sweetalert.js
+++ src/public/js/sweetalert.js
@@ -1,11 +1,13 @@
 // 회원가입 성공 메세지
 function alertRegister() {
     Swal.fire({
-        text: "회원가입이 성공되었습니다.",
+        text: "성공적으로 회원가입 되었습니다.<br>가입하신 정보로 다시 로그인해주세요.",
         icon: "success",
         showCancelButton: true,
         confirmButtonColor: "#2E5543",
-        confirmButtonText: "로그인"
+        cancleButtonColor: '#BC5539',
+        confirmButtonText: "로그인",
+        cancleButtonText: "닫기",
     }).then((result) => {
         if (result.isConfirmed) {
             window.location = "/user";
@@ -51,3 +53,26 @@ function alertChange() {
         timer: 1500,
     });
 }
+
+// 아이디 찾기
+function alertFindId() {
+  Swal.fire({
+    position: "top-center",
+    icon: "warning",
+    text: "입력하신 정보로 가입된 아이디를 찾을 수 없습니다.",
+    showConfirmButton: false,
+    timer: 1500,
+  })
+}
+
+// 비밀번호 찾기
+function alertFindPw() {
+  Swal.fire({
+    position: "top-center",
+    icon: "warning",
+    text: "존재하지 않는 정보입니다.",
+    showConfirmButton: false,
+    timer: 1500,
+  })
+}
+
